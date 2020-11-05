6f89e7047fa2ee606697f5f70301ba18274bd0f4
Annalisa <annalisadesantis770@gmail.com>
2020-11-05 18:25:21 +0100

    milestone 2

--- main.js
+++ main.js
@@ -97,21 +97,52 @@ $(document).ready(function() {
         },
     ];
 
-    // Creo un ciclo foreach per ciclare l'array icone
+    // Creo un array di colori
+    let colori = ["#5f9ea0", "#d891ef","#0bda51"];
+
+    // Creo un array di tipi di icona vuoto
+    const tipi_icone = [];
+
+    // Faccio un ciclo per individuare i tipi di icone presenti negli oggetti
     icone.forEach((icona) => {
 
-        // Destrutturo il singolo oggetto per ricavare le chiavi come varibiali
-        const {nome, prefisso, famiglia} = icona;
+        // Destrutturo la chiave tipo per usarla come variabile
+        const {tipo} = icona;
+
+        // Creo un if per verificare che il tipo letto dal ciclo non sia già presente nell'array vuoto che abbiamo creato prima (così da evitare doppioni di tipo)
+        if(!tipi_icone.includes(tipo)) {
+            tipi_icone.push(tipo);
+        }
+    });
+
+
+    // Creo un ciclo foreach per ciclare l'array icone
+    icone.forEach((icona) => {
+        // Applico la funzione che abbiamo precedentemente creato per stampare le icone in pagina (compresa di destrutturazione chiavi, indice del colore e template literal)
+        stampo_icone(icona); 
+    });
+
+
+    // Creo una funzione generica per stampare le icone in pagina con il loro indice di colore
+    function stampo_icone(element){
+
+        // Destrutturo le chiavi di ogni oggetto per renderele varibili
+        const {nome, prefisso, famiglia, tipo} = element;
+
+        // Creo una nuova varibile che indica l'indice dell'array tipi icone
+        const tipo_index = tipi_icone.indexOf(tipo);
+
+        // Creo una nuova variabile che dell'array colori prende l'indice dall'array tipo
+        const colori_icone = colori[tipo_index];
 
         // Stampo in pagina le icone con l'aiuto delle varibili ricavate dalla destrutturazione
         $(".container-all-icons").append(`
            <div class="icone">
-              <i class="${famiglia} ${prefisso}${nome} fa-2x"></i>
+              <i class="${famiglia} ${prefisso}${nome} fa-2x" style="color:${colori_icone}"></i>
               <span>${nome}</span>
            </div>
        `);
-
-    });
+    }
 
 
 });
